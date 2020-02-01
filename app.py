55634bde21ea3fd0e99c160dd0d9474869b63a2a
Javier <moubayyed47@hotmail.com>
2020-02-01 10:24:16 +0100

    endpoint two with body added

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from routing import endpoint_one
+from routing import endpoint_one, endpoint_two
 
 app = Flask(__name__)
 app.register_blueprint(endpoint_one)
+app.register_blueprint(endpoint_two)
 
 if __name__ == '__main__':
     app.run()
