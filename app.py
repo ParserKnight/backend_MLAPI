d4ce8c716fb7418732215efd195e214a579df93c
Javier M.R <moubayyed47@hotmail.com>
2020-02-22 19:27:08 +0100

    app fix just one blueprint for now

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
-from routing import endpoint_one, endpoint_two
+from routing import base_api
 
 app = Flask(__name__)
-app.register_blueprint(endpoint_one)
-app.register_blueprint(endpoint_two)
+app.register_blueprint(base_api)
 
 if __name__ == '__main__':
     app.run()
