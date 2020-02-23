82542b001d2c22be692a31b5eb78f710c5e272d0
Javier M.R <moubayyed47@hotmail.com>
2020-02-23 20:40:00 +0100

    validator added

--- validators.py
+++ validators.py
@@ -1,60 +1,41 @@
-
-
-
-
+from exceptions.exceptions import BadParameter
 
 class Parser():
+
     def __init__(self,body):
         self.body = body
-        self.greenparam = {
-            STATUS : (None, [0,1,2,3,4,5,C,X])
-            NAME_EDUCATION_TYPE : (str, ["Academic degree", "Higher education,Incomplete higher", "Lower secondary,Secondary / secondary special"])
-            NAME_INCOME_TYPE : [Commercial associate, Pensioner,state servant ,Student,Working ]
-            CODE_GENDER : [F , M]
-            NAME_FAMILY_STATUS : [Civil marriage, Married , Separated, Single / not married,Widow ]
-            NAME_HOUSING_TYPE : [Co-op apartment, House / apartment, Municipal apartment , Office apartment, Rented apartment, With parent]
-            CNT_CHILDREN : int 0-19
-            FLAG_OWN_CAR : (bool,None)
-            FLAG_MOBIL : bool
-            FLAG_WORK_PHONE : bool
-            FLAG_PHONE : bool
-            FLAG_EMAIL : bool
-            CTM_FAM_MEMBERS = int 1 a 20
-            FLAG_OWN_REALTY : bool
-            DAYS_BIRTH : int
-            DAYS_EMPLOYED : int
-            MONTHS_BALANCE : int 
-            AMT_INCOME_TOTAL : float
-            OCCUPATION_TYPE = 
+        self.green = {
+            "STATUS" : (str, ["0", "1", "2", "3", "4", "5", "C", "X"]),
+            "NAME_EDUCATION_TYPE": (str, ["Academic degree", "Higher education,Incomplete higher", "Lower secondary,Secondary / secondary special"]),
+            "NAME_INCOME_TYPE": (str, ["Commercial associate", "Pensioner", "state servant", "Student", "Working"]),
+            "CODE_GENDER": (str, ["F", "M"]),
+            "NAME_FAMILY_STATUS": (str, ["Civil marriage", "Married", "Separated", "Single / not married","Widow" ]),
+            "NAME_HOUSING_TYPE": (str, ["Co-op apartment", "House / apartment", "Municipal apartment" , "Office apartment", "Rented apartment", "With parent"]),
+            "CNT_CHILDREN": (int, None),
+            "FLAG_OWN_CAR": (bool, None),
+            "FLAG_MOBIL": (bool, None),
+            "FLAG_WORK_PHONE": (bool, None),
+            "FLAG_PHONE": (bool, None),
+            "FLAG_EMAIL": (bool, None),
+            "CTM_FAM_MEMBERS":(int, None),
+            "FLAG_OWN_REALTY": (bool, None),
+            "DAYS_BIRTH": (int, None),
+            "DAYS_EMPLOYED": (int, None),
+            "MONTHS_BALANCE": (int, None),
+            "AMT_INCOME_TOTAL": (float, None),
+            "OCCUPATION_TYPE" : (str, ["Accountants","Cleaning staff","Cooking staff","Core staff","Drivers","HR staff","High skill tech staff", "IT staff","Laborers","Low-skill Laborers","Managers","Medicine staff","Private service staff", "Realty agents", "Sales staff","Secretaries","Security staff","Waiters/barmen staff"])
         }
-         
 
-    def validate():
+    def validate(self):
         """Function that validate a body input"""
 
-        if not self.body:
-            return False
+        self.body = {key:value for key,value in self.body.items() if key in self.green.keys()}
+        ers = ["{} is not a {} value".format(k, self.green.get(k)[0]) for k, v in self.body.items() if not isinstance(v, self.green.get(k)[0])]
+        [ers.append("{} argument missing".format(k)) for k,v in self.green.items() if k not in self.body.keys()]
+        [ers.append("{} is invalid, it must be one of the following {} ".format(k,self.green.get(k)[1])) for k, v in self.body.items() if self.green.get(k)[1] and v not in self.green.get(k)[1]]
 
-        errors = list()
-
-        #todo esto puede ir en dos lineas
-        for key, value in self.body:
-            
-            if not isinstance(value, type(self.greenparam.get(key)[0])):
-                errors.append({"msg": "{} is not a {} value".format(key, type(self.greenparam.get(key)[0]))})
-
-            if self.greenparam.get(key)[1]:
-                if value not in self.greenparam.get(key)[1]
-                    errors.append({"msg":"{} is invalid, it must be one of the following values {} ".format(key,type(self.greenparam.get(key)[1]))})
-        #####
-        
-        if errors:
-            raise BadParameter(msg=", ".join(errors.values()))
-
-class BadParameter(Exception):
-    def __init__(self,msg,*args,**kwargs):
-        self.msg=None
+        if ers: 
+            raise BadParameter(msg=", ".join(ers))
 
 
-    #def traduction():
-    # For the future, token traduction
+
