07aae01b8a44e4e027cdb723425ee5ac05f62520
Javier M.R <moubayyed47@hotmail.com>
2020-02-23 20:39:29 +0100

    handler updated with badParameter exception

--- handlers.py
+++ handlers.py
@@ -1,16 +1,23 @@
-
-
 import logging
+from exceptions.exceptions import BadParameter
 
 def errorHandler(func):
-    def run(*args,**kwargs):
+    """Handler for exceptions on endpoint"""
+
+    def wrapper(*args,**kwargs):
+        """function wrapper"""
+
         try:
             return func(*args,**kwargs)
-        #except BadParameter as e:
-        #    return Error_response(msg=e.msg)
+
+        except BadParameter as e:
+            #add error model
+            return ({"error_msg":e.msg}), 400
+
         except Exception as e:
             raise e
-    return run
+
+    return wrapper
 
 
 
